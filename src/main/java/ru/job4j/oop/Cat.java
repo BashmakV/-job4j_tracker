444430cd9e5502d4b5d7c21bfeea184e9030a787
BashmakV <dolka-lenka@mail.ru>
2022-12-14 12:12:54 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,33 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        Cat black = new Cat();
+
+        black.eat("fish");
+        gav.eat("kotleta");
+
+        gav.giveNick("gav");
+        black.giveNick("black");
+
+        gav.show();
+        black.show();
     }
 }
