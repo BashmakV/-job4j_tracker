ccc2e366541bd0f4602273833a20919e6750b5b6
BashmakV <dolka-lenka@mail.ru>
2023-02-01 19:26:20 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -4,7 +4,9 @@ public class HtmlReport extends TextReport {
     @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>"
-                + "<br/>" +
+                +
+                "<br/>"
+                +
                 "<span>" + body + "</span>";
     }
 }
