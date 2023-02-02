0287f4eb967d0638cfd528d9103584aa84f47416
BashmakV <dolka-lenka@mail.ru>
2023-02-02 17:51:36 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() { }
 
@@ -13,6 +18,7 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+
     }
 
     public int getId() {
@@ -29,5 +35,18 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public class StartUI {
+        public static void main(String[] args) {
+            Item date = new Item();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            String currentDateTimeFormat = date.getCreated().format(formatter);
+            System.out.println(currentDateTimeFormat);
+        }
     }
 }
