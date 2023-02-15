4dd4af74fb5de2d03af60eb935166a160765bcef
BashmakV <dolka-lenka@mail.ru>
2023-02-15 16:36:55 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -27,13 +27,14 @@ public class ShopDrop {
         for (int i = 0; i < products.length - 1; i++) {
             if (products[i] == null) {
                 for (i = index; i < products.length - 1; i++) {
-                    if (products[i + 1] != null) {
-                        products[i] = products[i + 1];
-                        products[i + 1] = null;
+                          products[i] = products[i + 1];
                     }
+                products[products.length - 1] = null;
                 }
             }
-        }
         return products;
+        }
+
     }
-}
+
+
