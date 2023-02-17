f4fdf7adc702a4bfa69d7f23ce9be3f60b4fdbea
BashmakV <dolka-lenka@mail.ru>
2023-02-17 15:23:11 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -1,19 +1,16 @@
 package ru.job4j.pojo;
-
 public class ShopDrop {
     public static void main(String[] args) {
         Product[] products = new Product[3];
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
         products[2] = new Product("Egg", 19);
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
+        for (Product product : products) {
             System.out.println(product.getName());
         }
         products[1] = products[2];
         products[2] = null;
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
+        for (Product product : products) {
             if (product != null) {
                 System.out.println(product.getName());
             } else {
@@ -24,17 +21,10 @@ public class ShopDrop {
 
     public static Product[] delete(Product[] products, int index) {
         products[index] = null;
-        for (int i = 0; i < products.length - 1; i++) {
-            if (products[i] == null) {
-                for (i = index; i < products.length - 1; i++) {
-                          products[i] = products[i + 1];
-                    }
-                products[products.length - 1] = null;
-                }
-            }
-        return products;
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
-
+        products[products.length - 1] = null;
+        return products;
     }
-
-
+}
