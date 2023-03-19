8c5e543cd71e53a8d0dbc34e11d3376a2019075a
BashmakV <dolka-lenka@mail.ru>
2023-03-19 20:20:48 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,8 +16,6 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
@@ -49,6 +47,8 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка удаления заявки.");
                 }
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
